aa71ecc23b356b215ecff86aae38091230425892
Xiaoru Li <dev@xiaoru.li>
2020-07-19 14:46:24 +0200

    lesson: Perform basic database queries

--- sample.ts
+++ sample.ts
@@ -3,10 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const sam = await prisma.profile.create({
-    data: { name: "Sam" },
-  });
-  console.log(sam);
+  // await prisma.profile.create({ data: { name: "Alice" } });
+  // await prisma.profile.create({ data: { name: "Alex" } });
+  // const entries = await prisma.profile.findMany({
+  //   where: { name: { contains: "A" } },
+  // });
+  // console.log(entries);
+
+  // const secondEntry = await prisma.profile.update({
+  //   where: { id: 2 },
+  //   data: { name: "Bob" },
+  // });
+  // console.log(secondEntry);
+
+  // await prisma.profile.updateMany({
+  //   where: { name: { contains: "A" } },
+  //   data: { name: "**REDACTED**" },
+  // });
+  // const entries = await prisma.profile.findMany({});
+  // console.log(entries);
+
+  const { count } = await prisma.profile.deleteMany({});
+  console.log(count + " entries deleted");
 
   await prisma.disconnect();
 }
